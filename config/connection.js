f16899738e1ff83f550ed2fded96f32ec93a9711
Chris <cbega1227@gmail.com>
2022-12-23 17:19:52 -0700

    Handling merge conflict.

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,11 @@
 // require in mongoose
 // set up connection to db
 // export connection
+const { connect, connection } = require("mongoose");
+
+connect("mongodb://localhost/developersApplications", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+module.exports = connection;
