725d9bb19ab13273529d2cf7842e28ffe7c595db
Chris <cbega1227@gmail.com>
2022-12-30 13:04:57 -0700

    Finished up User schema... virutal isn't tested

--- models/User.js
+++ models/User.js
@@ -1,11 +1,54 @@
 //require in mongoose
+const { Schema, model } = require("mongoose");
 
 // create new userSchema with the values :
-// 1. username - string, unique, required, trimmed
-// 2. email - string, required, unique, match a valid email
-// 3. thoughts - array of _id values referencing Thought model
-// 4. friends - array of _id values references the User model
-// 5. toJSON - getters true, id false
+const userSchema = new Schema(
+  {
+    userName: {
+      type: String,
+      unique: true,
+      require: true,
+      trimmed: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      require: true,
+      // validate :
+      // match :
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
 
 // Schema settings -
 // Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
+userSchema
+  .virtual("friendCount")
+  .get(function () {
+    return `${friends.length}`;
+  })
+  .set(function (count) {
+    this.set(count);
+  });
+
+const User = model("user", userSchema);
+module.exports = User;
+// Matching validation - https://stackoverflow.com/questions/18022365/mongoose-validate-email-syntax
