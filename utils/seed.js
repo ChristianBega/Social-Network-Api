b7b91e4391a9f1ef29ea5928b7d9cb2135491a28
Chris <cbega1227@gmail.com>
2022-12-30 13:21:11 -0700

    Created pseudo code for seed.js

---
+++ utils/seed.js
@@ -0,0 +1,22 @@
+// Require in connection
+// require in models
+// require in data
+
+// connection.on ?????
+// connection.once ?????{
+
+//Drop existing users
+//Drop existing thoughts
+
+//Create empty array to store USERS && THOUGHTS
+
+//Add USERS to collection - insertMany
+//ADD THOUGHTS to collection - insertMany
+
+//}
+
+
+//connection.on - https://mongoosejs.com/docs/connections.html
+//connection.once -
+
+// on vs once - when you use 'once' it signifies that the event will be called only once i.e the first time the event occurred like here in this case the first time when the connection is opened ,it will not occur once per request but rather once when the mongoose connection is made with the db while the 'on' signifies the event will be called every time that it occurred
