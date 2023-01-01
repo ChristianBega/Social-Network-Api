3dad5c4d0de114714073b3db31026f14b438dd5c
Chris <cbega1227@gmail.com>
2023-01-01 10:24:56 -0700

    Working on user & thought controllers.

--- controllers/thoughtsConnection.js
+++ controllers/thoughtsConnection.js
@@ -6,3 +6,29 @@
 // 2. POST (create) thoughts to db
 // 3. PUT (update) thoughts from db by ID
 // 4. DELETE (remove) thoughts from db by ID
+
+const Thought = require("../models/thought");
+
+module.exports = {
+  getThought(req, res) {
+    Thought.find()
+      .then((thoughts) => res.json(thoughts))
+      .catch((err) => res.status(500).json(err));
+  },
+  getSingleThought(req, res) {
+    Thought.findOne({ _id: req.params.ID }); //pass in {id : req.params.thoughtId}, {}}
+    //.then
+    //.catch
+  },
+  createThought(req, res) {
+    Thought.create(req.body);
+    //.then
+    //.catch
+  },
+  updateThought(req, res) {
+    Thought.findOneAndUpdate({}); // pass in
+  },
+  deleteThought(req, res) {
+    Thought.findOneAndRemove({}); //pass in id req.params.userId
+  },
+};
--- controllers/userConnection.js
+++ controllers/userConnection.js
@@ -1,25 +1,25 @@
-
 const User = require("../models/User");
 
 module.exports = {
   getUsers(req, res) {
-    User.find();
-    //.then
-    //.catch
+    User.find()
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json({ message: err.message }));
   },
   getSingleUser(req, res) {
-    User.findOne({}); //pass in {id : req.params.userId}, {}}
-    //.then
-    //.catch
+    User.findOne({ _id: req.params.userId })
+      .then((user) => (!user ? res.status(404).json({ message: "No user with that ID" }) : res.json(user)))
+      .catch((err) => res.status(500).json({ message: err.message }));
   },
   createUser(req, res) {
-    User.create(req.body);
-    //.then
-    //.catch
+    User.create(req.body)
+      .then((user) => res.status(201).json(user))
+      .catch((err) => {
+        res.status(400).json({ message: err.message });
+      });
   },
   updateUser(req, res) {
-    User.findOneAndUpdate({}); // pass in 
-
+    User.findOneAndUpdate({}); // pass in
   },
   deleteUser(req, res) {
     User.findOneAndRemove({}); //pass in id req.params.userId
